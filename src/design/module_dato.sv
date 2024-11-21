8d5c13ab324505fcd4af1c6b9c09e6b48f14a147
Pablo Elizondo Espinoza <147214546+pabloelizondo17@users.noreply.github.com>
2024-11-20 22:34:25 -0600

    Update module_dato.sv

--- src/design/module_dato.sv
+++ src/design/module_dato.sv
@@ -1,3 +1,11 @@
+// ------------Tecnológico de Costa Rica-----------
+// Escuela de Ingeniería Electrónica: Diseño Lógico
+
+// Subsistema de captura de datos
+// Pablo Elizondo Espinoza
+// Eduardo Tencio Solano
+// Karina Quiros Avila
+
 module module_dato (
 
     input logic clk,
